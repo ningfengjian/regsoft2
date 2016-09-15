df0b5f080024d713b82b174d83de66ca41acf277
ningfengjian <1173990817@qq.com>
2016-09-15 15:19:20 +0800

    1
    1

---
+++ frmMainForm .Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace SoftReg
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.skinButton1 = new CCWin.SkinControl.SkinButton();
+            this.MachineCode = new CCWin.SkinControl.SkinTextBox();
+            this.BaseText = new CCWin.SkinControl.SkinWaterTextBox();
+            this.RB_aqy = new System.Windows.Forms.RadioButton();
+            this.RB_yk = new System.Windows.Forms.RadioButton();
+            this.RB_mg = new System.Windows.Forms.RadioButton();
+            this.RB_sh = new System.Windows.Forms.RadioButton();
+            this.MachineCode.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 10F);
+            this.label1.Location = new System.Drawing.Point(14, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "账号密码";
+            // 
+            // skinButton1
+            // 
+            this.skinButton1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.skinButton1.BackColor = System.Drawing.Color.Transparent;
+            this.skinButton1.ControlState = CCWin.SkinClass.ControlState.Normal;
+            this.skinButton1.DownBack = global::SoftReg.Properties.Resources.SkinButtonDownBack;
+            this.skinButton1.DrawType = CCWin.SkinControl.DrawStyle.Img;
+            this.skinButton1.Location = new System.Drawing.Point(180, 161);
+            this.skinButton1.MouseBack = global::SoftReg.Properties.Resources.SkinButtonMouseBack;
+            this.skinButton1.Name = "skinButton1";
+            this.skinButton1.NormlBack = global::SoftReg.Properties.Resources.SkinButtonNormlBack;
+            this.skinButton1.Size = new System.Drawing.Size(83, 34);
+            this.skinButton1.TabIndex = 3;
+            this.skinButton1.Text = "获取账号";
+            this.skinButton1.UseVisualStyleBackColor = false;
+            this.skinButton1.Click += new System.EventHandler(this.GetMachineCode);
+            // 
+            // MachineCode
+            // 
+            this.MachineCode.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.MachineCode.BackColor = System.Drawing.Color.Transparent;
+            this.MachineCode.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.MachineCode.Icon = null;
+            this.MachineCode.IconIsButton = false;
+            this.MachineCode.IconMouseState = CCWin.SkinClass.ControlState.Normal;
+            this.MachineCode.Location = new System.Drawing.Point(77, 50);
+            this.MachineCode.Margin = new System.Windows.Forms.Padding(0);
+            this.MachineCode.MinimumSize = new System.Drawing.Size(28, 28);
+            this.MachineCode.MouseBack = null;
+            this.MachineCode.MouseState = CCWin.SkinClass.ControlState.Normal;
+            this.MachineCode.Name = "MachineCode";
+            this.MachineCode.NormlBack = null;
+            this.MachineCode.Padding = new System.Windows.Forms.Padding(5);
+            this.MachineCode.Size = new System.Drawing.Size(373, 28);
+            // 
+            // MachineCode.BaseText
+            // 
+            this.MachineCode.SkinTxt.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.MachineCode.SkinTxt.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.MachineCode.SkinTxt.Font = new System.Drawing.Font("微软雅黑", 9.75F);
+            this.MachineCode.SkinTxt.Location = new System.Drawing.Point(5, 5);
+            this.MachineCode.SkinTxt.Name = "BaseText";
+            this.MachineCode.SkinTxt.Size = new System.Drawing.Size(363, 18);
+            this.MachineCode.SkinTxt.TabIndex = 0;
+            this.MachineCode.SkinTxt.WaterColor = System.Drawing.Color.FromArgb(((int)(((byte)(127)))), ((int)(((byte)(127)))), ((int)(((byte)(127)))));
+            this.MachineCode.SkinTxt.WaterText = "点击\"获取账号\"生成账号";
+            this.MachineCode.SkinTxt.TextChanged += new System.EventHandler(this.MachineCode_SkinTxt_TextChanged);
+            this.MachineCode.TabIndex = 4;
+            // 
+            // BaseText
+            // 
+            this.BaseText.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.BaseText.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.BaseText.Font = new System.Drawing.Font("微软雅黑", 9.75F);
+            this.BaseText.Location = new System.Drawing.Point(5, 5);
+            this.BaseText.Name = "BaseText";
+            this.BaseText.Size = new System.Drawing.Size(363, 18);
+            this.BaseText.TabIndex = 0;
+            this.BaseText.WaterColor = System.Drawing.Color.FromArgb(((int)(((byte)(127)))), ((int)(((byte)(127)))), ((int)(((byte)(127)))));
+            this.BaseText.WaterText = "点击\"生成注册码\"生成注册码";
+            // 
+            // RB_aqy
+            // 
+            this.RB_aqy.AutoSize = true;
+            this.RB_aqy.Location = new System.Drawing.Point(87, 98);
+            this.RB_aqy.Name = "RB_aqy";
+            this.RB_aqy.Size = new System.Drawing.Size(59, 16);
+            this.RB_aqy.TabIndex = 9;
+            this.RB_aqy.TabStop = true;
+            this.RB_aqy.Text = "爱奇艺";
+            this.RB_aqy.UseVisualStyleBackColor = true;
+            // 
+            // RB_yk
+            // 
+            this.RB_yk.AutoSize = true;
+            this.RB_yk.Location = new System.Drawing.Point(263, 98);
+            this.RB_yk.Name = "RB_yk";
+            this.RB_yk.Size = new System.Drawing.Size(71, 16);
+            this.RB_yk.TabIndex = 10;
+            this.RB_yk.TabStop = true;
+            this.RB_yk.Text = "优酷土豆";
+            this.RB_yk.UseVisualStyleBackColor = true;
+            // 
+            // RB_mg
+            // 
+            this.RB_mg.AutoSize = true;
+            this.RB_mg.Location = new System.Drawing.Point(87, 130);
+            this.RB_mg.Name = "RB_mg";
+            this.RB_mg.Size = new System.Drawing.Size(53, 16);
+            this.RB_mg.TabIndex = 11;
+            this.RB_mg.TabStop = true;
+            this.RB_mg.Text = " 芒果";
+            this.RB_mg.UseVisualStyleBackColor = true;
+            // 
+            // RB_sh
+            // 
+            this.RB_sh.AutoSize = true;
+            this.RB_sh.Location = new System.Drawing.Point(263, 130);
+            this.RB_sh.Name = "RB_sh";
+            this.RB_sh.Size = new System.Drawing.Size(47, 16);
+            this.RB_sh.TabIndex = 12;
+            this.RB_sh.TabStop = true;
+            this.RB_sh.Text = "搜狐";
+            this.RB_sh.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(460, 236);
+            this.Controls.Add(this.RB_sh);
+            this.Controls.Add(this.RB_mg);
+            this.Controls.Add(this.RB_yk);
+            this.Controls.Add(this.RB_aqy);
+            this.Controls.Add(this.MachineCode);
+            this.Controls.Add(this.skinButton1);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "各大视频网站VIP账号密码获取器";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MachineCode.ResumeLayout(false);
+            this.MachineCode.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private CCWin.SkinControl.SkinButton skinButton1;
+        private CCWin.SkinControl.SkinTextBox MachineCode;
+        private CCWin.SkinControl.SkinWaterTextBox BaseText;
+        private System.Windows.Forms.RadioButton RB_aqy;
+        private System.Windows.Forms.RadioButton RB_yk;
+        private System.Windows.Forms.RadioButton RB_mg;
+        private System.Windows.Forms.RadioButton RB_sh;
+    }
+}
+
---
+++ frmMainForm .cs
@@ -0,0 +1,302 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using CCWin;
+using Microsoft.Win32;
+using System.Security.Cryptography;
+using System.Xml;
+using System.Net;
+using System.IO;
+using System.Diagnostics;
+using System.Threading;
+
+namespace SoftReg
+{
+    public partial class Form1 : Skin_Mac
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            
+        }
+        SoftReg softReg = new SoftReg();
+
+
+
+
+
+        public string md5(string str)
+        {
+            MD5 m = new MD5CryptoServiceProvider();
+            byte[] s = m.ComputeHash(UnicodeEncoding.UTF8.GetBytes(str));
+            return BitConverter.ToString(s);
+        }
+
+        public string xxm(string urlpath,string path,string nodepath)
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+            string ActivityId = "";
+            try
+            {
+                xmlDoc.Load(@urlpath);
+                //xmlDoc.Load(@"");
+                //读取Activity节点下的数据。SelectSingleNode匹配第一个Activity节点  
+                // XmlNode root = xmlDoc.SelectSingleNode("/root/result");//当节点Workflow带有属性是，使用SelectSingleNode无法读取     
+                XmlNode root = xmlDoc.SelectSingleNode(path);
+                ActivityId = (root.SelectSingleNode(nodepath)).InnerText;
+                
+                
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("网络是否连接"); //显示错误信息  
+            }
+            return ActivityId;
+
+        }
+
+        public string HttpPost(string Url, string postDataStr)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(Url);
+            request.Method = "POST";
+            request.ContentType = "application/x-www-form-urlencoded";
+            //request.Headers.Add("User_Agent", "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.4 (KHTML, like Gecko) Chrome/22.0.1229.79 Safari/537.4");
+            //request.Headers["User-Agent"] = "onlyhuiyuan";
+            //request.Headers.Add("User-Agent", "onlyhuiyuan");
+            //request.Headers.Add("clientmarket", "1");
+            request.ContentLength = Encoding.UTF8.GetByteCount(postDataStr);
+            Stream myRequestStream = request.GetRequestStream();
+            StreamWriter myStreamWriter = new StreamWriter(myRequestStream, Encoding.GetEncoding("gb2312"));
+            myStreamWriter.Write(postDataStr);
+            myStreamWriter.Close();
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            Stream myResponseStream = response.GetResponseStream();
+            StreamReader myStreamReader = new StreamReader(myResponseStream, Encoding.GetEncoding("utf-8"));
+           string retString = myStreamReader.ReadToEnd();
+            myStreamReader.Close();
+            myResponseStream.Close();
+            return retString;
+        }
+
+        private void GetMachineCode(object sender, EventArgs e)
+        {
+            //string result = "zzz2188185";
+            //string bytes = md5(result);
+            
+            string MachineStr = softReg.GetMNum();//磁盘信息
+
+            string urlpath = "http://api.k780.com:88/?app=life.time&appkey=10003&sign=b59bc3ef6191eb9f747dd4e83c99f2a4&format=xml";
+            string path = "/root/result";
+            string nodepath = "timestamp";
+            string erre = xxm(urlpath, path, nodepath);
+            int ti = Convert.ToInt32(erre);
+            //if (erre == "") {  }
+            //else { ti = Convert.ToInt32(erre); }//当前时间
+            //int timeupdate = ti;
+            //int timeupdate = ti - 18000;
+            //MessageBox.Show(timeupdate.ToString());
+            //string Ps = MachineStr + bytes.Replace("-", "");
+
+            string urlrs = "http://www.nandada.com/";
+            if(!(RB_aqy.Checked || RB_yk.Checked || RB_mg.Checked || RB_sh.Checked) ){ MessageBox.Show("请选择一项"); }
+            else{
+                if (HttpPost(urlrs + "exit.php", "MachineStr=" + MachineStr) == "no") {
+                    //HttpPost(urlrs + "inser.php", "MachineStr=" + MachineStr);
+                    if (RB_aqy.Checked)
+                    {
+                        string urlpath2 = urlrs + "xmlaqy.php";
+                        string path2 = "/users/user";
+                        string nodepath2 = "name";
+                        string aqy_zh = xxm(urlpath2, path2, nodepath2);
+                        MachineCode.SkinTxt.Text = aqy_zh;
+                        //HttpPost(urlrs + "d.php", "MachineStr=" + MachineStr);
+                        if (aqy_zh == "")
+                        {
+                            MessageBox.Show("领取更多账号，点击加入群【爱奇艺会员分享批发】：http://jq.qq.com/?_wv=1027&k=2EJVSc9");
+                            System.Diagnostics.Process.Start("http://jq.qq.com/?_wv=1027&k=2EJVSc9");
+                        }
+
+                    }
+
+                    else if (RB_yk.Checked)
+                    {
+                        string urlpath2 = urlrs + "xmlyk.php";
+                        string path2 = "/users/user";
+                        string nodepath2 = "name";
+                        string aqy_zh = xxm(urlpath2, path2, nodepath2);
+                        MachineCode.SkinTxt.Text = aqy_zh;
+                        //HttpPost(urlrs + "d.php", "MachineStr=" + MachineStr);
+                        if (aqy_zh == "")
+                        {
+                            MessageBox.Show("领取更多账号，点击加入群【爱奇艺会员分享批发】：http://jq.qq.com/?_wv=1027&k=2EJVSc9");
+                            System.Diagnostics.Process.Start("http://jq.qq.com/?_wv=1027&k=2EJVSc9");
+                        }
+
+                    }
+
+                    else if (RB_mg.Checked)
+                    {
+                        string urlpath2 = urlrs + "xmlmg.php";
+                        string path2 = "/users/user";
+                        string nodepath2 = "name";
+                        string aqy_zh = xxm(urlpath2, path2, nodepath2);
+                        MachineCode.SkinTxt.Text = aqy_zh;
+                        //HttpPost(urlrs + "d.php", "MachineStr=" + MachineStr);
+                        if (aqy_zh == "")
+                        {
+                            MessageBox.Show("领取更多账号，点击加入群【爱奇艺会员分享批发】：http://jq.qq.com/?_wv=1027&k=2EJVSc9");
+                            System.Diagnostics.Process.Start("http://jq.qq.com/?_wv=1027&k=2EJVSc9");
+                        }
+
+                    }
+                    else if (RB_sh.Checked)
+                    {
+                        string urlpath2 = urlrs + "xmlsh.php";
+                        string path2 = "/users/user";
+                        string nodepath2 = "name";
+                        string aqy_zh = xxm(urlpath2, path2, nodepath2);
+                        MachineCode.SkinTxt.Text = aqy_zh;
+                        //HttpPost(urlrs + "d.php", "MachineStr=" + MachineStr);
+                        if (aqy_zh == "")
+                        {
+                            MessageBox.Show("领取更多账号，点击加入群【爱奇艺会员分享批发】：http://jq.qq.com/?_wv=1027&k=2EJVSc9");
+                            System.Diagnostics.Process.Start("http://jq.qq.com/?_wv=1027&k=2EJVSc9");
+                        }
+
+                    }
+
+                }
+                else {
+                    string urlpath2 = urlrs + "timeinfo.php?MachineStr=" + MachineStr;
+                    string pathu = "/users/user";
+                    string nodepathu = "name";
+                    string erre2 = xxm(urlpath2, pathu, nodepathu);
+                    int ti2 = Convert.ToInt32(erre2);
+                    if (ti - ti2 > 18000)
+                    {
+                        if (RB_aqy.Checked)
+                        {
+                            string urlpathaqy = urlrs + "xmlaqy.php?MachineStr=" + MachineStr;
+                            string pathaqy = "/users/user";
+                            string nodepathaqy = "name";
+                            string aqy_zh = xxm(urlpathaqy, pathaqy, nodepathaqy);
+                            MachineCode.SkinTxt.Text = aqy_zh;
+                            Thread.Sleep(3000);
+                            MessageBox.Show("点击链接加入群【爱奇艺会员分享批发】：http://jq.qq.com/?_wv=1027&k=2EJVSc9");
+                            System.Diagnostics.Process.Start("http://jq.qq.com/?_wv=1027&k=2EJVSc9");
+                            if (aqy_zh == "")
+                            {
+                                MessageBox.Show("领取更多账号，点击加入群【爱奇艺会员分享批发】：http://jq.qq.com/?_wv=1027&k=2EJVSc9");
+                                System.Diagnostics.Process.Start("http://jq.qq.com/?_wv=1027&k=2EJVSc9");
+                            }
+                            //HttpPost(urlrs + "d.php", "MachineStr=" + MachineStr);
+                        }
+
+                        else if (RB_yk.Checked)
+                        {
+                            string urlpathyk = urlrs + "xmlyk.php?MachineStr=" + MachineStr;
+                            string pathyk = "/users/user";
+                            string nodepathyk = "name";
+                            string aqy_zh = xxm(urlpathyk, pathyk, nodepathyk);
+                            MachineCode.SkinTxt.Text = aqy_zh;
+                            //HttpPost(urlrs + "d.php", "MachineStr=" + MachineStr);
+                            Thread.Sleep(3000);
+                            MessageBox.Show("点击链接加入群【爱奇艺会员分享批发】：http://jq.qq.com/?_wv=1027&k=2EJVSc9");
+                            System.Diagnostics.Process.Start("http://jq.qq.com/?_wv=1027&k=2EJVSc9");
+                            if (aqy_zh == "")
+                            {
+                                MessageBox.Show("领取更多账号，点击加入群【爱奇艺会员分享批发】：http://jq.qq.com/?_wv=1027&k=2EJVSc9");
+                                System.Diagnostics.Process.Start("http://jq.qq.com/?_wv=1027&k=2EJVSc9");
+                            }
+                        }
+
+                        else if (RB_mg.Checked)
+                        {
+                            string urlpathmg = urlrs + "xmlmg.php?MachineStr=" + MachineStr;
+                            string pathmg = "/users/user";
+                            string nodepathmg = "name";
+                            string aqy_zh = xxm(urlpathmg, pathmg, nodepathmg);
+                            MachineCode.SkinTxt.Text = aqy_zh;
+                            // HttpPost(urlrs + "d.php", "MachineStr=" + MachineStr);
+                            Thread.Sleep(3000);
+                            MessageBox.Show("点击链接加入群【爱奇艺会员分享批发】：http://jq.qq.com/?_wv=1027&k=2EJVSc9");
+                            System.Diagnostics.Process.Start("http://jq.qq.com/?_wv=1027&k=2EJVSc9");
+                            if (aqy_zh == "")
+                            {
+                                MessageBox.Show("领取更多账号，点击加入群【爱奇艺会员分享批发】：http://jq.qq.com/?_wv=1027&k=2EJVSc9");
+                                System.Diagnostics.Process.Start("http://jq.qq.com/?_wv=1027&k=2EJVSc9");
+                            }
+                        }
+                        else if (RB_sh.Checked)
+                        {
+                            string urlpathsh = urlrs + "xmlsh.php?MachineStr=" + MachineStr;
+                            string pathsh = "/users/user";
+                            string nodepathsh = "name";
+                            string aqy_zh = xxm(urlpathsh, pathsh, nodepathsh);
+                            MachineCode.SkinTxt.Text = aqy_zh;
+                            //HttpPost(urlrs + "d.php", "MachineStr=" + MachineStr);
+                            Thread.Sleep(3000);
+                            MessageBox.Show("点击链接加入群【爱奇艺会员分享批发】：http://jq.qq.com/?_wv=1027&k=2EJVSc9");
+                            System.Diagnostics.Process.Start("http://jq.qq.com/?_wv=1027&k=2EJVSc9");
+                            if (aqy_zh == "")
+                            {
+                                MessageBox.Show("领取更多账号，点击加入群【爱奇艺会员分享批发】：http://jq.qq.com/?_wv=1027&k=2EJVSc9");
+                                System.Diagnostics.Process.Start("http://jq.qq.com/?_wv=1027&k=2EJVSc9");
+                            }
+                        }
+
+                    }
+                    else { MessageBox.Show("请五个小时候再获取"); }
+
+                }
+            }
+
+            //string urlpath= "http://api.k780.com:88/?app=life.time&appkey=10003&sign=b59bc3ef6191eb9f747dd4e83c99f2a4&format=xml";
+            //string path = "/root/result";
+            //string nodepath = "timestamp";
+            //string erre = xxm(urlpath, path, nodepath);
+            //int ti = 0;
+            //if (erre == "") { MessageBox.Show("网络是否连接"); }
+            //else{ti = Convert.ToInt32(erre); }
+
+            //int timeupdate = ti - 18000;
+            //MessageBox.Show(timeupdate.ToString());
+            //string result = "zzz2188185";
+            //string bytes = md5(result);
+            //string MachineStr = softReg.GetMNum();
+            //string Ps = MachineStr + bytes.Replace("-", "");
+
+
+        }
+
+
+        private void MachineCode_SkinTxt_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string urlrs = "http://www.nandada.com/";
+            string urlpathmg = urlrs + "update.php";
+            string pathmg = "/users/user";
+            string nodepathmg = "name";
+            string aqy_zh = xxm(urlpathmg, pathmg, nodepathmg);
+            //MachineCode.SkinTxt.Text = aqy_zh;
+            if(aqy_zh=="update"){
+
+                Process.Start("update.exe");
+                Application.Exit();
+
+            }
+            
+        }
+    }
+}
